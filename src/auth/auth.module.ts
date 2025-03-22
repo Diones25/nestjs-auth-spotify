c519875591bfade5c11722869beadf5bc01da1b6
Diones <pereiradiones987@gmail.com>
2025-03-22 14:36:35 -0300

    configurando module para o token jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -10,7 +10,8 @@ import { SpotifyStrategy } from './strategies/spotify.strategy';
   imports: [
     PassportModule.register({ defaultStrategy: 'spotify' }),
     JwtModule.register({
-      secret: "3dd53afbe800ffcf016df7a3d1201eabb84d62d8fab22900324a56101a3603a5"
+      secret: "3dd53afbe800ffcf016df7a3d1201eabb84d62d8fab22900324a56101a3603a5",
+      signOptions: { expiresIn: '1h' }, // Tempo de vida do JWT (1 hora)
     })
   ],
   providers: [SpotifyStrategy, AuthService],
