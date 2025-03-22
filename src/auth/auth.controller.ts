88f18c0256c7a5ae7a2def5a1097c779a6123d58
Diones <pereiradiones987@gmail.com>
2025-03-22 14:16:20 -0300

    controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @Get('spotify')
+  @UseGuards(AuthGuard('spotify'))
+  async spotifyLogin() { }
+  
+  @Get('spotify/callback')
+  @UseGuards(AuthGuard('spotify'))
+  async spotifyLoginCallback(@Req() req) {
+    // Após a autenticação, o usuário é redirecionado para esta rota
+    // O usuário autenticado é armazenado em req.user
+    return req.user;
+  }
+}
