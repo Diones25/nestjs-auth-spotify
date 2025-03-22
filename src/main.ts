db5c30e2aae86623a5ca9715b37a25dd51aac205
Diones <pereiradiones987@gmail.com>
2025-03-22 15:28:51 -0300

    configurando sessão

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    session({
+      secret: "63dadae5120d7e86be3d8cf7b350e6b8763fe36c", //chave secreta para assinar a sessão
+      resave: false, //Evita regravar a sessão se não houver alterações
+      saveUninitialized: false, // Não salva sessões não inicializadas
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24, // 1 dia de duração do cookie
+        httpOnly: true, // O cookie só é acessível via HTTP (não é acessível via JavaScript)
+      }
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
