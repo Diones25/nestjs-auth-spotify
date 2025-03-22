75c3435d311ca75c9492304b078f522161b4ed78
Diones <pereiradiones987@gmail.com>
2025-03-22 15:20:51 -0300

    atualizando controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,25 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Query, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Request, Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
   @Get('spotify')
   @UseGuards(AuthGuard('spotify'))
-  async spotifyLogin() { }
+  async login() { }
   
   @Get('spotify/callback')
   @UseGuards(AuthGuard('spotify'))
-  async spotifyLoginCallback(@Req() req) {
-    // Após a autenticação, o usuário é redirecionado para esta rota
-    // O usuário autenticado é armazenado em req.user
-    return req.user;
+  async callback(@Req() req: Request, @Res() res: Response) {
+    // Aqui você pode redirecionar o usuário ou retornar os tokens
+    const { accessToken, refreshToken } = req.user as any;
+
+    // Redireciona o usuário para outra rota após a autenticação
+    res.redirect(`/auth/profile?accessToken=${accessToken}&refreshToken=${refreshToken}`);
+  }
+
+  @Get('profile')
+  async profile(@Query('accessToken') accessToken: string, @Query('refreshToken') refreshToken: string) {
+    return { accessToken, refreshToken };
   }
 }
